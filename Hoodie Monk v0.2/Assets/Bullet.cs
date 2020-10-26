aee88552bf5f9f8c9a67b2481bba694652536d37
Kase Osei <811007@learn.vsb.bc.ca>
2020-10-26 13:16:48 -0700

    Buncha stuff, enemy effects and such

--- Hoodie Monk v0.2/Assets/Bullet.cs
+++ Hoodie Monk v0.2/Assets/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
 
     public float speed = 20f;
+    public int damage = 5;
     public Rigidbody2D rb;
     public ParticleSystem impactEffect;
     public ParticleSystem travelingFire;
@@ -18,6 +19,11 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D hitInfo) // THIS FUNC CALLS TWICE SOMETIMES, CLAIM ITS CRITS FOR NOW, FIX THIS LATER.
     {
+        EnemyController enemy = hitInfo.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
         boomPs = Instantiate(impactEffect, transform.position, transform.rotation) as ParticleSystem;
         Debug.Log(hitInfo.name);
         Destroy(boomPs.gameObject, 1f);
@@ -28,8 +34,9 @@ public class Bullet : MonoBehaviour
     {
         //// This splits the particle off so it doesn't get deleted with the parent
         //travelingFire.transform.SetParent(null);
-        rb.GetComponent<Renderer>().enabled = false;
-        Destroy(rb);
+        if (rb != null){
+            rb.GetComponent<Renderer>().enabled = false;
+            Destroy(rb);}
         travelingFire.Stop();
         Destroy(gameObject, 0.7f);
     }
--- Hoodie Monk v0.2/Assets/EnemyController.cs
+++ Hoodie Monk v0.2/Assets/EnemyController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3.0f;
     public bool vertical;
+    public float speed = 3.0f;
     public float changeTime = 3.0f;
+    public ParticleSystem enemyDeathParticles;
+    private ParticleSystem enemyDeathPS;
+
+    public Sprite idleSprite;
+    public Sprite hitSprite;
 
     Rigidbody2D rigidbody2D;
+    public int enemyhealth = 15;
     float timer;
     int direction = 1;
 
@@ -43,5 +49,29 @@ public class EnemyController : MonoBehaviour
         }
 
         rigidbody2D.MovePosition(position);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        StartCoroutine(TakeHit());
+        enemyhealth -= damage;
+        if (enemyhealth <= 0)
+        {
+            Die();
+        }
+
+    void Die()
+        {
+            enemyDeathPS = Instantiate(enemyDeathParticles, transform.position, transform.rotation) as ParticleSystem;
+            Destroy(enemyDeathPS.gameObject, 3f);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator TakeHit() //Contents not in 'Update()' because it can't be paused
+    {
+        rigidbody2D.GetComponent<SpriteRenderer>().sprite = hitSprite; //Sets the sprite to the taking damage sprite
+        yield return new WaitForSeconds(0.3f); //Pause (makes it look better)
+        rigidbody2D.GetComponent<SpriteRenderer>().sprite = idleSprite; //Sets the sprite back to the normal sprite
     }
 }
